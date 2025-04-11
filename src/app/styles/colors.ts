42100fda7e6cbe6cd0069250327adc4e36d8e8ed
Tnalxmsk <alstnwkd990@naver.com>
2025-04-12 01:07:51 +0900

    :art: style: base Theme 설정

--- src/app/styles/colors.ts
+++ src/app/styles/colors.ts
@@ -1,5 +1,22 @@
 import { ColorsTypes } from '@shared/types';
 
 export const colors: ColorsTypes = {
-
+  yellow: '#FFDB57',
+  orange300: '#ED9508',
+  orange50: '#FFF6C7',
+  blue300: '#3E78E2',
+  blue50: '#E6F0FC',
+  gray900: '#18181b',
+  gray800: '#27272a',
+  gray700: '#3f3f46',
+  gray600: '#52525b',
+  gray500: '#71717a',
+  gray400: '#a1a1aa',
+  gray300: '#d4d4d8',
+  gray200: '#e4e4e7',
+  gray100: '#f4f4f5',
+  gray50: '#fafafa',
+  primary: '#59D9CC',
+  sub: '#BAFFF8',
+  sub2: '#E0FFFC',
 };
