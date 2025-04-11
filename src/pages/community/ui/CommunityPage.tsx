21686277ba70dd3883e1ab08638de3ca5ea2cda5
Tnalxmsk <alstnwkd990@naver.com>
2025-04-12 01:09:34 +0900

    :art: style: Home, Mission, Community, MyPage 컴포넌트 추가

---
+++ src/pages/community/ui/CommunityPage.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@shared/ui/NavBar.tsx";
+
+export const CommunityPage = () => {
+  return (
+    <div>
+      <h1>Community Page</h1>
+      <p>This is the community page.</p>
+      <NavBar />
+    </div>
+  );
+};
