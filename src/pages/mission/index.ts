1ca2ccec6f41187d241a5ba798c1bc90df01c326
Tnalxmsk <alstnwkd990@naver.com>
2025-04-12 20:33:22 +0900

    :sparkles: feat: 미션 기록, 완료 페이지 index.ts 추가

--- src/pages/mission/index.ts
+++ src/pages/mission/index.ts
@@ -1,1 +1,3 @@
 export { MissionPage } from '@mission/ui/MissionPage';
+export { MissionRecordWritePage } from '@mission/ui/MissionRecordWritePage';
+export { MissionCompletePage } from '@mission/ui/MissionCompletePage';
