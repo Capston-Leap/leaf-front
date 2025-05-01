cc8d3b42490f334a9784d86779805c2840fbac7f
Tnalxmsk <alstnwkd990@naver.com>
2025-05-01 19:16:04 +0900

    :sparkles: feat: Onboarding index.ts 추가

--- src/pages/onboarding/index.ts
+++ src/pages/onboarding/index.ts
@@ -1,3 +1,4 @@
-export { OnboardingPage } from '@onboarding/ui/OnboardingPage';
+export { InitPage } from '@onboarding/ui/InitPage.tsx';
 export { LoginPage } from '@onboarding/ui/LoginPage';
 export { SignupPage } from '@onboarding/ui/SignupPage';
+export { OnboardingPage } from '@onboarding/ui/OnboardingPage';
