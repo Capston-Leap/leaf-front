d4f7b98bf19fd5c4ee5bf59a8ca34aed63188310
Tnalxmsk <alstnwkd990@naver.com>
2025-05-01 17:16:06 +0900

    :refactor: loginform props 제거

--- src/pages/onboarding/components/LoginForm.tsx
+++ src/pages/onboarding/components/LoginForm.tsx
@@ -4,8 +4,8 @@ import styled from "styled-components";
 const LoginForm = () => {
   return (
     <FormContainer>
-      <CustomInput label="아이디" type="text" name="text" placeholder="아이디를 입력해 주세요" required={true} />
-      <CustomInput label="비밀번호" type="password" name="password" placeholder="비밀번호를 입력해 주세요" required={true} />
+      <CustomInput label="아이디" type="text" placeholder="아이디를 입력해 주세요" required={true} />
+      <CustomInput label="비밀번호" type="password" placeholder="비밀번호를 입력해 주세요" required={true} />
       <Button $isValid={true} onClick={() => null} type="submit">로그인</Button>
     </FormContainer>
   );
@@ -17,7 +17,7 @@ const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 20%;
-  gap: 30px;
+  gap: 20px;
   padding: 0 16px;
 `;
 
