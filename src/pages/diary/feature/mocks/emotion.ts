b17b10aa90c35ed0559172ef2280340540f71b08
Tnalxmsk <alstnwkd990@naver.com>
2025-05-07 21:01:12 +0900

    :sparkles: feat: 감정 유형 데이터

---
+++ src/pages/diary/feature/mocks/emotion.ts
@@ -0,0 +1,17 @@
+import ImgAnger from "@img/img-anger.png";
+import ImgSurprise from "@img/img-surprise.png";
+import ImgHurt from "@img/img-hurt.png";
+import ImgEmbarrassment from "@img/img-embarrassment.png";
+import ImgDelight from "@img/img-delight.png";
+import ImgUnrest from "@img/img-unrest.png";
+import ImgSorrow from "@img/img-sorrow.png";
+
+export const emotion = [
+  { id: 1, type: 'SORROW', img: ImgSorrow },
+  { id: 2, type: 'ANGER', img: ImgAnger },
+  { id: 3, type: 'HURT', img: ImgHurt },
+  { id: 4, type: 'DELIGHT', img: ImgDelight },
+  { id: 5, type: 'UNREST', img: ImgUnrest },
+  { id: 6, type: 'SURPRISE', img: ImgSurprise },
+  { id: 7, type: 'EMBARRASSMENT', img: ImgEmbarrassment },
+];
