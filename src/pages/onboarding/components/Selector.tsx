a7d162929d5c0f7bd96fc51de583164d12ee3e20
Tnalxmsk <alstnwkd990@naver.com>
2025-05-15 22:17:42 +0900

    :sparkles: feat: 챗봇 설정

--- src/pages/onboarding/components/Selector.tsx
+++ src/pages/onboarding/components/Selector.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import { useState } from "react";
+import { ChatbotSettingRequest } from "@shared/types/request/user.ts";
 
 interface SelectorProps {
-  type: 'gender' | 'personality';
+  type: 'gender' | 'Character';
+  handleChatbotSetting: (type: keyof ChatbotSettingRequest, value: string) => void
 }
 
 const selectorType = {
@@ -16,16 +18,27 @@ const selectorType = {
   },
 };
 
-const Selector = ({ type }: SelectorProps) => {
+const Selector = ({ type, handleChatbotSetting }: SelectorProps) => {
   const [selectedNumber, setSelectedNumber] = useState(0);
   const options: string[] = type === 'gender' ? selectorType.gender.options : selectorType.personality.options;
+
+  const handleSelect = (index: number) => {
+    setSelectedNumber(index + 1);
+    const value = type === 'gender'
+      ? index === 0 ? 'M' : 'F'
+      : index === 0 ? 'F' : 'T';
+    handleChatbotSetting(type === 'Character' ? 'character' : type, value);
+  };
 
   return (
     <div>
       <p>{type === 'gender' ? '성별' : '성격'}</p>
       <CardList style={{ display: 'flex', gap: '16px' }}>
         {options.map((option, index) => (
-          <Card key={index} $isSelected={selectedNumber === index + 1} onClick={() => setSelectedNumber(index + 1)}>{option}</Card>
+          <Card key={index} $isSelected={selectedNumber === index + 1} onClick={() => {
+            handleSelect(index)
+          }}
+          >{option}</Card>
         ))}
       </CardList>
     </div>
