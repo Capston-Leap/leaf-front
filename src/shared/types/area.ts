74e9fa1b869c0bb55cbdcfe249821f82fdf82e6f
Tnalxmsk <alstnwkd990@naver.com>
2025-05-15 22:10:20 +0900

    :sparkles: feat: 영역 타입 설정

--- src/shared/types/area.ts
+++ src/shared/types/area.ts
@@ -1,7 +1,7 @@
 export const AreaType: Record<string, string> = {
-  DAILY_LIFE: '일상생활',
-  SELF_MANAGEMENT: '자기 관리',
-  MONEY_MANAGEMENT: '돈 관리',
+  LIFE: '일상생활',
+  SELF: '자기 관리',
+  MONEY: '돈 관리',
   SOCIETY: '사회 진출',
   ALL: '모든 기술 마스터 !!!',
 };
